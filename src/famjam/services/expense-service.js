842b3566c18582291a8463d9bb2585e14088170a
Rishita <reddy.ri@northeastern.edu>
2022-04-20 01:11:17 -0400

    edit-expense changes

--- src/famjam/services/expense-service.js
+++ src/famjam/services/expense-service.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
-
-
-
 const API_BASE = 'http://localhost:4000/famjam/group1/expense';
 const EXPENSE_API = `${API_BASE}`;
 
@@ -20,4 +17,26 @@ export const deleteExpense = async (expense) => {
     const response = await axios
         .delete(`${EXPENSE_API}/${expense._id}`);
     return response.data;
+}
+
+
+export const updateExpense = async (expense) => {
+    console.log("inside service status");
+    const response = await axios.put(`${EXPENSE_API}/${expense._id}`, expense);
+    return response.data;
+}
+
+
+
+
+export function getExpense() {
+    const response = axios.get(EXPENSE_API);
+    const expense = response.data;
+    let total = 0;
+    let i;
+    for (i = 0; i < expense.length; i++) {  //loop through the array
+        total += expense[i].amount;  //Do the math!
+    }
+    console.log(total);
+    return total;
 }
